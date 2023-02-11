18514541cac9d1528f7242f1798ad7a92f07e048
Baptiste Langlade <baptouuuu@gmail.com>
2023-02-11 13:45:10 +0100

    add functional test

--- tests/HaltTest.php
+++ tests/HaltTest.php
@@ -5,6 +5,8 @@ namespace Tests\Innmind\Async\TimeWarp;
 
 use Innmind\Async\TimeWarp\Halt;
 use Innmind\Mantle\{
+    Forerunner,
+    Source\Predetermined,
     Suspend,
     Suspend\Synchronous,
 };
@@ -28,5 +30,34 @@ class SuspendTest extends TestCase
         $halt(new Second(2));
 
         $this->assertGreaterThanOrEqual(2, \microtime(true) - $start);
+    }
+
+    public function testFunctional()
+    {
+        $queue = new \SplQueue;
+        $clock = new Clock;
+        $source = Predetermined::of(
+            static function(Suspend $suspend) use ($clock, $queue) {
+                $halt = Halt::of($clock, $suspend);
+
+                $halt(new Second(2));
+
+                $queue->push('World !');
+            },
+            static function(Suspend $suspend) use ($clock, $queue) {
+                $halt = Halt::of($clock, $suspend);
+
+                $halt(new Second(1));
+
+                $queue->push('Hello ');
+            },
+        );
+
+        Forerunner::of($clock)(null, $source);
+
+        $this->assertSame(
+            ['Hello ', 'World !'],
+            \iterator_to_array($queue),
+        );
     }
 }
